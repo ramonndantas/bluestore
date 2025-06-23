a1fa09ff59f0d880d873209283c1416885b73acd
RC230821 <ramondantas007@gmail.com>
2025-06-23 11:26:41 -0300

    ecommerce

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector(".navbar");
+      const menuButton = document.querySelector(".menu-button");
+
+      menuButton.addEventListener("click", ()=>{
+         navbar.classList.toggle("show-menu");
+      });
+
+      
